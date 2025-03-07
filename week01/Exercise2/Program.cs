c983fe0c8b324674e793b4a567480f30febef96d
Efraín <efrainperezdelgado@gmail.com>
2025-03-07 20:43:29 +0100

    completed core requirements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //Get user's grade
+        Console.Write("Please, enter your grade percentage (0 - 100): ");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+
+        char gradeLetter;
+
+        if (grade >= 90)
+        {
+            gradeLetter = 'A';
+        }
+
+        else if (grade >= 80)
+        {
+            gradeLetter = 'B';
+        }
+
+        else if (grade >= 70)
+        {
+            gradeLetter = 'C';
+        }
+
+        else if (grade >= 60)
+        {
+            gradeLetter = 'D';
+        }
+
+        else
+        {
+            gradeLetter = 'F';
+        }
+
+        Console.WriteLine("Your grade is " + gradeLetter);
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed.");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry, you have failed the class.");
+        }
     }
 }
