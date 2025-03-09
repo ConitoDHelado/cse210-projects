53eec8cad6ae27a17a9e05ce7ebe5779382e791a
Efraín <efrainperezdelgado@gmail.com>
2025-03-09 21:09:28 +0100

    completed excersice

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Formats.Asn1;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string answer;
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 11);
+
+            int guess;
+            int numberGuesses = 0;
+            do
+            {
+                numberGuesses++;
+                Console.Write("What is your guess?: ");
+
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+            } while (guess != magicNumber);
+
+
+
+            Console.WriteLine($"You guessed it! It took you {numberGuesses} guesses");
+            Console.Write("Do you want to play again?(Y/N): ");
+            answer = Console.ReadLine().ToLower();
+        } while (answer == "y" || answer == "yes");
     }
 }
