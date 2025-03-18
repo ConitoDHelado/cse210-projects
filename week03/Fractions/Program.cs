9c29516cef982459f9f839798063d0b00fc4056d
Efraín <efrainperezdelgado@gmail.com>
2025-03-18 18:48:35 +0100

    Completed Fraction Activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        f1.SetTop(3);
+        f1.SetBottom(7);
+        int num = f1.GetTop();
+        int den = f1.GetBottom();
+        Console.WriteLine($"the numerator is {num} and denominator is {den}");
+
+        // Why doesn't this work??
+        string fraction = f1.GetFractionString();
+        double decimalValue = f1.GetDecimalValue();
+        Console.WriteLine($"{fraction} = {decimalValue}");
+
+        // Console.WriteLine(f1.GetFractionString());
+        // Console.WriteLine(f1.GetDecimalValue());
+
     }
 }
