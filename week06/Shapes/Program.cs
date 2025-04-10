49f205c425bea630f5a7ac478edd14b1be5b9599
Efraín <efrainperezdelgado@gmail.com>
2025-04-10 10:47:33 +0200

    Completed shape activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square s = new Square("red", 2);
+        Rectangle r = new Rectangle("blue", 2, 4);
+        Circle c = new Circle("yellow", 3);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s);
+        shapes.Add(r);
+        shapes.Add(c);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+
     }
 }
